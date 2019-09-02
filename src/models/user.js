ef9e3b8c937f59e5dc35e0fb697be5f35371662d
Shasha <1078201502@qq,com>
2019-09-02 17:14:35 +0800

    123

--- src/models/user.js
+++ src/models/user.js
@@ -11,6 +11,7 @@ export default {
   effects: {
     *fetch(_, { call, put }) {
       const response = yield call(queryUsers);
+      console.log(response)
       yield put({
         type: 'save',
         payload: response.result,
